1085d304452e3956e1a9ee8e0fdea603c0005916
Pedzenn <129620727+Pedzenn@users.noreply.github.com>
2025-06-10 22:00:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(page_title="Magic Analytics", layout="wide")
 
@@ -13,5 +14,17 @@ if uploaded_file is not None:
 
     st.subheader("Resumo das colunas numéricas")
     st.write(df.describe())
+
+    # Top 10 cartas mais usadas nos decks
+    st.subheader("Top 10 Cartas Mais Utilizadas nos Decks")
+
+    top_usadas = df.sort_values(by="Quantidade em Deck", ascending=False).head(10)
+    fig_usadas = px.bar(top_usadas, x="Nome", y="Quantidade em Deck",
+                        title="Top 10 Cartas com Maior Quantidade em Deck",
+                        labels={"Quantidade em Deck": "Qtd em Deck"},
+                        text="Quantidade em Deck")
+    fig_usadas.update_traces(textposition='outside')
+    st.plotly_chart(fig_usadas)
+
 else:
     st.info("Envie um arquivo .csv para começar.")
